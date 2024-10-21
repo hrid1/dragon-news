5cb55d6ff9df9be528093c020b3024cd8fcc5e73
hrid1 <hridoycse1@gmail.com>
2024-10-22 00:11:45 +0600

    Header Section Done

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,20 @@
+import moment from "moment";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
-    return (
-        <div>
-            <h3>This si header</h3>
-        </div>
-    );
+  return (
+    <div className="text-center space-y-2.5 mt-10 mb-6">
+      <div>
+        <img className="mx-auto" src={logo} alt="" />
+      </div>
+      <p className="text-gray-500">Journalism Without Fear of Favoure</p>
+      {/* <h3>{moment().format("dddd, MMMM D, YYYY")}</h3> */}
+      <h3>
+        <span className="font-semibold">{moment().format("dddd")},</span>
+        {moment().format(" MMMM D, YYYY")}
+      </h3>
+    </div>
+  );
 };
 
 export default Header;
