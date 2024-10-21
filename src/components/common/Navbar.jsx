f71b87fe8bdeb0575edc51c3eba2f348be7a3fe0
hrid1 <hridoycse1@gmail.com>
2024-10-22 02:19:14 +0600

    Navabar and Latest news Done

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,10 +1,64 @@
+import { Link, NavLink } from "react-router-dom";
+import user from "../../assets/user.png";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>This is navbar</h2>
+  const NavLinks = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/about"}>About</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/Career"}>Career</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {NavLinks}
+          </ul>
         </div>
-    );
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{NavLinks}</ul>
+      </div>
+      <div className="navbar-end flex gap-2.5 items-center">
+        <img className="w-8 h-8 rounded-full" src={user} alt="nai" />
+
+        <Link
+          to={"login"}
+          className="bg-red-200 py-2  font-semibold rounded px-6"
+        >
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,31 @@
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header";
 import Navbar from "../../components/common/Navbar";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
-    return (
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Header></Header>
+      {/* <marquee behavior="" direction="">mell</marquee> */}
+      <div className="flex items-center bg-gray-300 p-2">
+        <button className="btn btn-primary rounded text-lg">Latest</button>
 
-        <div className="max-w-6xl mx-auto">
-            <Header></Header>
-            <Navbar></Navbar>
-            <h1>This is home</h1>
+        <Marquee speed={80} >
+          I can be a React component, multiple React components, or just some
+          components, or just some text.
+        </Marquee>
+      </div>
+      <Navbar></Navbar>
+      <h1>This is home</h1>
 
-            <div>
-                
-            </div>
-            
-        </div>
-    );
+      <div className="grid grid-cols-4 gap-6">
+        <div className=" col-span-1 border bg-white">left</div>
+        <div className=" col-span-2 border bg-white">center</div>
+        <div className=" col-span-1 border bg-white">left</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
